5c9becff64631066ebc6a5031eac8b20ce9cab75
Denis Sbitny <sbitny@mail.ru>
2025-10-12 02:49:10 +0300

    Обновил фокусы

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -9,7 +9,7 @@ const Textarea = React.forwardRef<
   return (
     <textarea
       className={cn(
-        "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm transition-colors transition-shadow duration-200 placeholder:text-muted-foreground focus-visible:border-transparent focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm transition-colors transition-shadow duration-200 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-[4px] focus-visible:ring-[rgba(0,0,0,0.18)] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         className
       )}
       ref={ref}
